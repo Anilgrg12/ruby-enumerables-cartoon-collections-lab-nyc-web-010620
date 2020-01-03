5b3386c62b987dec30ae27c23b9c890d50b1cef3
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-03 11:22:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@
 # end # Your code here
 
 def summon_captain_planet (long_planeteer_calls)
-  long_planeteer_calls.map|word|
+  long_planeteer_calls.map do |word|
   word.capitalize + "!"
+end
 end
   
   
