be68f4a71b1abf7ba6f094494304c07edd509556
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-03 11:13:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+s# code an argument here
+  def roll_call_dwarves(array)
+    array.each_with_index do |name,index|
+      puts "#{index+1}. #{name}"
+
+# end # Your code here
 end
 
 def summon_captain_planet# code an argument here
