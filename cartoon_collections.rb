54858e5e44cab50df8eae48dd07e20b076bb2779
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-03 11:28:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,12 @@ end
 # code an argument here
   # Your code here
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  array.any? do|word|
+  word.length>4
+end
+  
+  # code an argument here
   # Your code here
 end
 
