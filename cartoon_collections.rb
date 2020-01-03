b33d63fa6eafb5fe164a45bf9461e43f40f84619
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-03 11:30:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,19 +12,15 @@ def summon_captain_planet (array)
   word.capitalize + "!"
 end
 end
-# code an argument here
-  # Your code here
+
 
 def long_planeteer_calls(array)
   array.any? do|word|
   word.length > 4
 end
-  
-  # code an argument here
-  # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
